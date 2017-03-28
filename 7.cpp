5246f4b152d7c644c59e0af0dc5a197a4c49cafe
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-03-28 07:51:05 -0500

    7 TERMINADO GRACIAS JAIME POR TANTO TRABAJO

--- 7.cpp
+++ 7.cpp
@@ -5,13 +5,23 @@
 #include <vector>
 using namespace std;
 
+int modulo(int a, int n){
+	int q, r;
+	q = a/n;
+	r = a - (q*n);
+	if (r<0){
+		r += n;
+	}
+	return r;
+}
+
 int inversus(vector<int> &cocientes, int mod){
 	int p_0 = 0, p_1 = 1, p, q;
 	for(int i = 0; i < cocientes.size()-1; i++){
 		q = cocientes.at(i);
 		p = p_0 - (p_1*q);
 		if (p<0)//p = modulo(p, mod);
-			p = p%mod;
+			p = modulo(p,mod);
 		p_0 = p_1;
 		p_1 = p;
 	}
@@ -27,14 +37,14 @@ private:
 		int a = this->publicKey, b, resul, modul = alfabeto.length(), modOriginal, prevMod = 1;
 		vector<int> results;
 		modOriginal = modul;
-		while(1){ 
+		while(1){
 			prevMod = modul % a; //prevmod = r
 			b = (modul - prevMod) / a;
 			results.push_back(b);
 			if (prevMod == 0)
 				break;
-			modul = a;	
-			a = prevMod; //euclides	
+			modul = a;
+			a = prevMod; //euclides
 		}
 		if (a != 1)
 			this->privateKey = 0;
@@ -47,16 +57,16 @@ public:
 	Cesar(int pubkey){
 		this -> alfabeto = "abcdefghijklmnopqrstuvwxyz";
 		this -> publicKey = pubkey;
-		privSetPrivKey(); 
+		privSetPrivKey();
 	}
 	//setters and getters
 	void setPublicKey(int pubkey) { this -> publicKey = pubkey; }
 	void setPrivateKey(int privkey) { this -> privateKey = privkey; }
 	int getPrivateKey() { return this->privateKey;}
 	int getPubKey() {return this->publicKey;}
-	//class functions	
-	string cifrar(string msj){
-		int i = 0, found;
+	//class functions
+	string cifrar(string cadena){
+		/*int i = 0, found;
 		string cifrado, letra;// = "";
 		for (i; i<msj.length(); i++){
 			letra = msj[i];
@@ -66,15 +76,24 @@ public:
 			}
 			found = alfabeto.find(letra);
 			found += this->privateKey;
-			if (found > alfabeto.length())			
+			if (found > alfabeto.length())
 				found = found % alfabeto.length();
-			cifrado += alfabeto[found];
-			
+			cifrado += alfabeto[found];*/
+    int i;
+    string letra;
+    for(i = 0; i <= cadena.size(); i++){
+        letra = cadena[i];
+        if (letra == " ")
+            continue;
+        else if (alfabeto.find(cadena[i])+privateKey >= alfabeto.size())
+            cadena[i] = alfabeto[modulo(alfabeto.find(cadena[i])+privateKey , alfabeto.size())];
+        else
+            cadena[i] = alfabeto[alfabeto.find(cadena[i])+privateKey];
+    }
+    return cadena;
 		}
-		return cifrado;
-	}
-	string descifrar(string cifrado){ 
-		int i = 0, found;
+	string descifrar(string cadena){
+		/*int i = 0, found;
 		string descifrado, letra;
 		for (i; i<cifrado.length(); i++){
 			letra = cifrado[i];
@@ -83,16 +102,100 @@ public:
 				continue;
 			}
 			found = alfabeto.find(letra);
-			found -= this->privateKey; 
+			found -= this->privateKey;
 			if (found < 0){
 				found = found % alfabeto.length();
 			}
 			descifrado += alfabeto[found];
 		}
 		return descifrado;
-	}
+	}*/
+	int i;
+    string letra;
+    for (i = 0; i <= cadena.size(); i++){
+        letra = cadena[i];
+        if (letra == " ")
+            continue;
+        if(privateKey - alfabeto.find(cadena[i]) > 0)
+            cadena[i] = alfabeto[modulo(alfabeto.find(cadena[i]) - privateKey , alfabeto.size())];
+        else
+            cadena[i] = alfabeto[alfabeto.find(cadena[i])-privateKey];
+    }
+    return cadena;
+    }
 };
 
+/*class Cesar
+{
+private:
+    int clave;
+    string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    int privateKey;
+	void privSetPrivKey(){ //private key setter, private function
+		int a = this->clave, b, resul, modul = alfabeto.length(), modOriginal, prevMod = 1;
+		vector<int> results;
+		modOriginal = modul;
+		while(1){
+			prevMod = modul % a; //prevmod = r
+			b = (modul - prevMod) / a;
+			results.push_back(b);
+			if (prevMod == 0)
+				break;
+			modul = a;
+			a = prevMod; //euclides
+		}
+		if (a != 1)
+			this->privateKey = 0;
+		else {
+			resul = inversus(results, modOriginal);
+			this->privateKey = resul;
+		}
+	}
+public:
+    void setPublicKey(int pubkey) { this -> clave = pubkey; }
+	void setPrivateKey(int privkey) { this -> privateKey = privkey; }
+	int getPrivateKey() { return this->privateKey;}
+	int getPubKey() {return this->clave;}
+Cesar(int clav)
+{
+    this->clave = clav;
+    privSetPrivKey();
+}int i;
+    for (i = 0; i <= cadena.size(); i++){
+        if(clave - alfabeto.find(cadena[i]) > 0)
+            cadena[i] = alfabeto[modulo(alfabeto.find(cadena[i]) - clave , alfabeto.size())];
+        else
+            cadena[i] = alfabeto[alfabeto.find(cadena[i])-clave];
+    }
+    return cadena;
+    }
+
+string cifrar(string cadena)
+{
+    int i;
+    for(i = 0; i <= cadena.size(); i++){
+        if (cadena[i] == " ")
+            continue;
+        else if (alfabeto.find(cadena[i])+clave >= alfabeto.size())
+            cadena[i] = alfabeto[modulo(alfabeto.find(cadena[i])+clave , alfabeto.size())];
+        else
+            cadena[i] = alfabeto[alfabeto.find(cadena[i])+clave];
+    }
+    return cadena;
+}
+
+string descifrar(string cadena)
+{
+    int i;
+    for (i = 0; i <= cadena.size(); i++){
+        if(clave - alfabeto.find(cadena[i]) > 0)
+            cadena[i] = alfabeto[modulo(alfabeto.find(cadena[i]) - clave , alfabeto.size())];
+        else
+            cadena[i] = alfabeto[alfabeto.find(cadena[i])-clave];
+    }
+    return cadena;
+    }
+};*/
 void cifrando(){
 	ifstream msjOriginal;
 	ofstream msjCifrado;
@@ -110,7 +213,7 @@ void cifrando(){
 	}
 	cout << "Mensaje cifrado.\n";
 	msjOriginal.close();
-	msjCifrado.close();	
+	msjCifrado.close();
 }
 
 void descifrando(){
@@ -126,7 +229,7 @@ void descifrando(){
 	Algoritmo.setPrivateKey(privateKey);
 	while(getline(msjCifrado, cifrado)){
 		descifrado = Algoritmo.descifrar(cifrado);
-		plainText << descifrado;
+		plainText << descifrado << endl;
 	}
 	cout << "Mensaje descifrado.\n";
 	msjCifrado.close();
