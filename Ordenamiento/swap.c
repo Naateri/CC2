e9543538592d0d5454203043acb56aec105eefd4
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-12 12:02:31 -0500

    swap

---
+++ Ordenamiento/swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *p, int *q){
+	int *temp;
+	*temp = *p;
+	*p = *q;
+	*q = *temp;
+}
+
+int main(){
+	int a = 3, b = 5;
+	printf("a = %d, b = %d\n", a, b);
+	swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+	printf("Este es mi programa!\n");
+	return 0;
+}
+
