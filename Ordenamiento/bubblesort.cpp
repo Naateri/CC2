7123a9fcc18670cca0e2b00035b8655bea0a6885
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-18 08:28:12 -0500

    Update bubblesort.cpp

--- Ordenamiento/bubblesort.cpp
+++ Ordenamiento/bubblesort.cpp
@@ -12,7 +12,12 @@ void swap(int *p, int *q){
 }
 
 int sizeOfArray(int *a, int *b){ //funciona mientras no se repitan elementos
-	return (b-a) + 1;
+	int i = 1;
+	while(*a != *b){
+		a = a+1;
+		i++;
+	}
+	return i;
 }
 
 void printArray(int *a, int n){
@@ -24,13 +29,13 @@ void printArray(int *a, int n){
 }
 
 void bubbleSort(int *a, int *b){
-	int *a0, *a1;
-	int n = sizeOfArray(a, b);
+	int *a0, *a1, *a_b; //a_b = a beginning
+	a_b = a;
 	bool status = true;
 	while (status){
 		status = false;
 		a0 = a; a1 = (a0 + 1);
-		for(int i = 0; i < n-1; i++){
+		for(int i = 0; a < b; i++, a++){
 			if (*a0 > *a1){
 				swap(a0, a1);
 				status = true;
@@ -38,7 +43,7 @@ void bubbleSort(int *a, int *b){
 			a0 = a1;
 			a1 = (a1+1);
 		}
-		n--;
+		a = a_b - 1;
 	}
 }
 
@@ -47,4 +52,5 @@ int main(){
 	bubbleSort(a, (a+9));
 	printArray(a, 10);
 }
+
 
