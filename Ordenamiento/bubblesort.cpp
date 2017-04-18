00f41121f22f6c06093264636e35ac812f7e0604
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-18 08:01:07 -0500

    bubblesort + funciones basicas

--- Ordenamiento/bubblesort.cpp
+++ Ordenamiento/bubblesort.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void swap(int *p, int *q){
+	int temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
+
+int sizeOfArray(int *a, int *b){ //funciona mientras no se repitan elementos
+	int i = 1;
+	while(*a != *b){
+		a = a+1;
+		i++;
+	}
+	return i;
+}
+
+void printArray(int *a, int n){
+	for (int i = 0; i < n; i++){
+		cout << *a << ", ";
+		a++;
+	}
+	cout << '\n';
+}
 
 void bubbleSort(int *a, int *b){
-  ;
+	int *a0, *a1;
+	int n = sizeOfArray(a, b);
+	bool status = true;
+	while (status){
+		status = false;
+		a0 = a; a1 = (a0 + 1);
+		for(int i = 0; i < n-1; i++){
+			if (*a0 > *a1){
+				swap(a0, a1);
+				status = true;
+			}
+			a0 = a1;
+			a1 = (a1+1);
+		}
+		n--;
+	}
 }
 
 int main(){
-  return 0;
- }
+	int a[] = {4, 6, 2, 12, 10, 3, 9, 11, 8, 16};
+	bubbleSort(a, (a+9));
+	printArray(a, 10);
+}
+
