e28dfdafba04693cd991d76737dd1fe00251d433
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-05-16 08:25:17 -0500

    Update rompecabezas.cpp

--- Rompecabezas/rompecabezas.cpp
+++ Rompecabezas/rompecabezas.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
+
+int rompecabezas[4][4];
 
 bool ordenado(int (*mat)[4]){
 	int *row = *mat;
@@ -17,4 +23,68 @@ bool ordenado(int (*mat)[4]){
 		mat++;
 	}
 	return 1;
+}
+
+///########################################################################################################
+
+
+void llenar_romp(){
+    int num = 1;
+    while (num != 16){
+        int i = rand()%4;
+        int j = rand()%4;
+        if(rompecabezas[i][j] == 0){
+            rompecabezas[i][j] = num;
+            num++;
+        }
+    }
+}
+
+void imprimir(){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            cout << rompecabezas[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void tecla(){
+    char cTecla;
+
+    cout << "Presione una tecla ..." << endl;
+
+    while(cTecla != 27)
+    {
+       cTecla = getch();
+       if(cTecla == 0)
+           cTecla = getch();
+       else
+           switch(cTecla)
+           {
+            case 72:
+                 cout << "Presiono Arriba" << endl;
+            break;
+
+            case 80:
+                 cout << "Presiono Abajo" << endl;
+            break;
+
+            case 75:
+                 cout << "Presiono izquierda" << endl;
+            break;
+
+            case 77:
+                 cout << "Presiono derecha" << endl;
+            break;
+            }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    llenar_romp();
+    imprimir();
+    tecla();
 }
