0b02a786275da7c6420f544ca6030fbf208867cd
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-06-27 08:05:29 -0500

    bolillero ya hecho

--- Tarea/dado.cpp
+++ Tarea/dado.cpp
@@ -3,51 +3,21 @@
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 int rdtsc(){
 	__asm__ __volatile__("rdtsc");
 }
 
-vector<int> MakeVector(){
+vector<int> MakeVector(int n){
 	vector<int> v;
-	for (int i = 1; i <= 75; i++)
+	for (int i = 1; i <= n; i++)
 		v.push_back(i);
 	return v;
 }
 
-class BolillasArray{
-private:
-	int _size;
-	int *array;
-	void resize(int x){
-		int *a;
-		int i;
-		a = new int[x];
-		this->_size = x;
-		for(i = 0; i< x; i++){
-			*(a+i) = *(array+i);
-		}
-		delete [] array;
-		array = a;
-	}
-public:
-	BolillasArray(){ //FIX
-		this->_size = 0;
-		int *a;
-		a = new int[0];
-		this->array = a;
-		//this->point = NULL;
-	}
-	BolillasArray(int bolillas[], int size){
-		int i;
-		this->_size = size;
-		this->array = new int[size];
-		for (i = 0; i< size; i++){
-			*(array+i) = *(bolillas+i);
-		}
-	}
-};
+const int n = 75;
 
 class Dado{
 public:
@@ -60,13 +30,15 @@ public:
 class Bolillero{
 private:
 	static vector<int> bolillas;
+    char bingo[5] = {'B','I','N','G','O'};
 public:
-	int operator()(){ ///n = numero de bolas
+	pair<int, char> operator()(int n){ ///n = numero de bolas
 		srand(rdtsc());
-		int num;
+		int num, gap = n/5;
+		pair<int, char> bola;
 		vector<int>::iterator it;
 		do{
-			num = 1 + rand()%75;
+			num = 1 + rand()%n;
 			if (bolillas.size() == 1){
 				break;
 			}
@@ -74,18 +46,27 @@ public:
 		}while(it == bolillas.end());
 		if (bolillas.size() > 1)
 			bolillas.erase(it);
-		return num;
+        bola.first = num;
+        if(num == n) {
+            bola.second = *(bingo+4);
+        } else if (num % gap == 0){
+            bola.second = *(bingo + (num / gap) - 1);
+        }
+        else bola.second = *(bingo + (num / gap));
+		return bola;
 	}
 };
 
-vector<int> Bolillero::bolillas = MakeVector();
+vector<int> Bolillero::bolillas = MakeVector(n);
 int main(int argc, char *argv[]) {
 	Dado A;
 	Bolillero B;
-	for (int i = 0; i < 75; i++){
-		cout << "i: " << i << " - "<< B() << endl;
+	pair<int, char> hola;
+	for (int i = 0; i < n; i++){
+        hola = B(n);
+		cout << /*"i: " << i << " - "<<*/ hola.second << "-" << hola.first << endl;
 	}
-	cout << A(6) << endl;
+	cout << "Dado: " << A(20) << endl;
 	return 0;
 }
 
